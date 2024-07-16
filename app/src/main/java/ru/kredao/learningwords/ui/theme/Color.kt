9f333c523e83c511e1d4d9b1ff229d2db9b13b15
Kredao <kredao2007@gmail.com>
2024-07-16 13:06:17 +0600

    create material theme and import icons

--- app/src/main/java/ru/kredao/learningwords/ui/theme/Color.kt
+++ app/src/main/java/ru/kredao/learningwords/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package ru.kredao.learningwords.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Black = Color(0xFF343434)
+val GrayWhite = Color(0xFFA4A4A4)
+val Black2 = Color(0xFF3F3F3F)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Gray = Color(0xFFF2F2F2)
+val Gray2 = Color(0xFF757575)
--- app/src/main/java/ru/kredao/learningwords/ui/theme/Type.kt
+++ app/src/main/java/ru/kredao/learningwords/ui/theme/Type.kt
@@ -2,33 +2,24 @@ package ru.kredao.learningwords.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import ru.kredao.learningwords.R
 
 // Set of Material typography styles to start with
+val myFontFamily = FontFamily(
+    Font(R.font.montserrat, FontWeight.Normal),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    displayLarge = TextStyle(
+        fontFamily = myFontFamily,
+        fontWeight = FontWeight.SemiBold,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayMedium = TextStyle(
+        fontFamily = myFontFamily,
+        fontWeight = FontWeight.Normal,
+    ),
 )
